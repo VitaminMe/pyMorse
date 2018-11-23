a15744ae300c438297e341bbae5d3e994606e39a
Chris Fleming <>
2018-11-23 10:41:39 -0800

    added some comments

--- pyMorse.py
+++ pyMorse.py
@@ -7,10 +7,11 @@
 The software will receive text as an input and produce the equivalent message
 in written morse code as the output
 '''
+# ToDo: Add all the text letters and morse code values to the dictionary.
 dictionary = {'s':'...', 'o': '---'}
 
+# Stores the output.
 morse_string = []
-text_string = (' ')
 
 text_string = input('Input text to convert to morse: ')
 
@@ -18,4 +19,5 @@ for letter in text_string:
     convert = dictionary[letter]
     morse_string.append(convert)
 
+# Fix the output to remove the [' '] marks.
 print(morse_string)
